05241c46769880498debe9bb799b0f7807e40c1d
Brian Zelip <bzelip@protonmail.com>
2022-05-15 11:11:45 -0400

    Add form toggle for get cover art workflow

---
+++ front/src/components/TheArtToggle.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps(['modelValue']);
+defineEmits(['update:modelValue']);
+</script>
+
+<template>
+  <fieldset class="inline-grid switch-grid-cols ml-5">
+    <label class="mr-3 italic small-caps">Get cover art next?</label>
+    <button
+      type="button"
+      :class="[
+        modelValue ? 'bg-green-500 border-green-500' : 'bg-black border-white',
+        'relative inline-flex flex-shrink-0 h-6 w-11 border-2 rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none',
+      ]"
+      @click="$emit('update:modelValue', !modelValue)"
+      role="switch"
+      aria-checked="false"
+    >
+      <span class="sr-only">Get cover artwork toggle</span>
+      <span
+        aria-hidden="true"
+        :class="[
+          modelValue ? 'translate-x-5' : 'translate-x-0',
+          'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200',
+        ]"
+      ></span>
+    </button>
+  </fieldset>
+</template>
--- front/src/components/TheEditForm.vue
+++ front/src/components/TheEditForm.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue';
 import { genres, countries, sizes, years } from '@/store.js';
 
 import FormInputText from './FormInputText.vue';
@@ -7,32 +8,72 @@ import FormMultiInputText from './FormMultiInputText.vue';
 import FormInputCheckboxGroup from './FormInputCheckboxGroup.vue';
 import FormSelect from './FormSelect.vue';
 import TheInlineNoveltyCheckboxes from './TheInlineNoveltyCheckboxes.vue';
+import TheArtToggle from './TheArtToggle.vue';
+
+const artIsEnabled = ref(true);
 </script>
 
 <template>
   <form method="POST" action="/add-record">
-    <FormInputText :label="{ for: 'title', text: 'Title' }" :input="{ id: 'title', name: 'title', required: true }">
+    <FormInputText
+      :label="{ for: 'title', text: 'Title' }"
+      :input="{ id: 'title', name: 'title', required: true }"
+    >
     </FormInputText>
-    <FormMultiInputText :fieldset="{ id: 'artists', class: 'flex items-end gap-x-2' }"
-      :label="{ for: 'artist', text: 'Artist' }" :input="{ id: 'artist', name: 'artist' }"></FormMultiInputText>
-    <FormMultiInputText :fieldset="{ id: 'labels', class: 'flex items-end gap-x-2' }"
-      :label="{ for: 'label', text: 'Label' }" :input="{ id: 'label', name: 'label' }"></FormMultiInputText>
-    <FormInputNumber :label="{ for: 'year', text: 'Year' }"
-      :input="{ id: 'year', name: 'year', min: years.min, max: years.max }"></FormInputNumber>
-    <FormInputCheckboxGroup :items="genres" :fieldset="{ id: 'genres' }" :legend="{ text: 'Genres' }"
-      :input="{ id: 'genre', name: 'genre' }" :label="{ for: 'genre' }"></FormInputCheckboxGroup>
-    <FormSelect :label="{ for: 'country', text: 'Country' }" :select="{ id: 'country', name: 'country' }"
+    <FormMultiInputText
+      :fieldset="{ id: 'artists', class: 'flex items-end gap-x-2' }"
+      :label="{ for: 'artist', text: 'Artist' }"
+      :input="{ id: 'artist', name: 'artist' }"
+    ></FormMultiInputText>
+    <FormMultiInputText
+      :fieldset="{ id: 'labels', class: 'flex items-end gap-x-2' }"
+      :label="{ for: 'label', text: 'Label' }"
+      :input="{ id: 'label', name: 'label' }"
+    ></FormMultiInputText>
+    <FormInputNumber
+      :label="{ for: 'year', text: 'Year' }"
+      :input="{ id: 'year', name: 'year', min: years.min, max: years.max }"
+    ></FormInputNumber>
+    <FormInputCheckboxGroup
+      :items="genres"
+      :fieldset="{ id: 'genres' }"
+      :legend="{ text: 'Genres' }"
+      :input="{ id: 'genre', name: 'genre' }"
+      :label="{ for: 'genre' }"
+    ></FormInputCheckboxGroup>
+    <FormSelect
+      :label="{ for: 'country', text: 'Country' }"
+      :select="{ id: 'country', name: 'country' }"
       :option="{ selectedMatch: 'United States of America' }"
-      :enums="{ type: 'object', options: countries, textProperty: 'alpha2', valueProperty: 'country' }"></FormSelect>
-    <FormSelect :label="{ for: 'size', text: 'Size' }" :select="{ id: 'size', name: 'size' }"
-      :option="{ selectedMatch: '12' }" :enums="{ type: 'string', options: sizes }"></FormSelect>
-    <FormInputText :label="{ for: 'color', text: 'Color' }" :input="{ id: 'color', name: 'color', default: 'black' }">
+      :enums="{
+        type: 'object',
+        options: countries,
+        textProperty: 'alpha2',
+        valueProperty: 'country',
+      }"
+    ></FormSelect>
+    <FormSelect
+      :label="{ for: 'size', text: 'Size' }"
+      :select="{ id: 'size', name: 'size' }"
+      :option="{ selectedMatch: '12' }"
+      :enums="{ type: 'string', options: sizes }"
+    ></FormSelect>
+    <FormInputText
+      :label="{ for: 'color', text: 'Color' }"
+      :input="{ id: 'color', name: 'color', default: 'black' }"
+    >
     </FormInputText>
     <TheInlineNoveltyCheckboxes></TheInlineNoveltyCheckboxes>
-    <FormInputNumber :label="{ for: 'copies', text: 'Copies' }"
-      :input="{ id: 'copies', name: 'copies', min: 1, value: 1 }"></FormInputNumber>
-    <button type="submit"
-      class="mt-4 ml-2 py-3 px-4 rounded-sm border-2 border-green-500 text-green-500 hover:bg-green-500 hover:text-black">ADD
-      RECORD</button>
+    <FormInputNumber
+      :label="{ for: 'copies', text: 'Copies' }"
+      :input="{ id: 'copies', name: 'copies', min: 1, value: 1 }"
+    ></FormInputNumber>
+    <button
+      type="submit"
+      class="mt-4 ml-2 py-3 px-4 w-28 rounded-sm border-2 border-green-500 text-green-500 hover:bg-green-500 hover:text-black"
+    >
+      {{ artIsEnabled ? 'GET ART' : 'SUBMIT' }}
+    </button>
+    <TheArtToggle v-model="artIsEnabled"></TheArtToggle>
   </form>
 </template>
